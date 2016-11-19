55ed8e25f45d7e2881e3038dfb34da1596f0314d
Yago Zacarias Gomes Coutinho Ribeiro <yzgcr@ug5c21.windows.cin.ufpe.br>
2016-11-19 15:42:50 -0300

    Verificando se tem contribuidor

--- TAIA.py
+++ TAIA.py
@@ -6,15 +6,14 @@ def request_repository(repository):
 	reque = requests.get('https://api.github.com/search/repositories?q='+repository)
 	if(reque.ok):
 		repoItem = json.loads(reque.text or reque.content)
-		print 'entrou'
-		import pdb;pdb.set_trace()
 		for project in repoItem['items']:
 			print 'repositorio: ' + project['full_name'],'\n'
 			constributors_get = requests.get(project['contributors_url']+AUTH)
-			constributors = json.loads(constributors_get.text or constributors_get.content)
-			for contributor in constributors:
-				print contributor['login'],contributor['contributions']
-			print '\n'
+			if constributors_get.text:
+				constributors = json.loads(constributors_get.text or constributors_get.content)
+				for contributor in constributors:
+					print contributor['login'],contributor['contributions']
+				print '\n'
 	else:
 		print 'fuu'
 		# print "Django repository created: " + repoItem['created_at']
