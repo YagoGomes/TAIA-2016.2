9ddb2b7ed2e4501592b3cdbb6d49b7a60cfa16ae
Yago Zacarias Gomes Coutinho Ribeiro <yzgcr@ug4c22.windows.cin.ufpe.br>
2016-11-26 14:22:26 -0300

    criando as funçoes de repositorios e contribuidores

--- TAIA_djeefther.py
+++ TAIA_djeefther.py
@@ -12,10 +12,9 @@ def request_repository(repository):
 			constributors_get = requests.get(project['contributors_url']+AUTH)
 			if constributors_get.text:
 				constributors = json.loads(constributors_get.text or constributors_get.content)
-				import pdb;pdb.set_trace()
-				# for contributor in constributors:
-				# 	print contributor['login'],contributor['contributions']
-				# print '\n'
+				for contributor in constributors:
+					print contributor['login'],contributor['contributions']
+				print '\n'
 	else:
 		print 'fuu'
 		# print "Django repository created: " + repoItem['created_at']
@@ -39,34 +38,26 @@ def getting_better_contributors(repoItem):
 	list_sorted = sorted(list_contri, key=lambda tup: tup[2], reverse=True)
 	return list_sorted[:2]
 
-def repository(owner,repo):
+def return_contri(owner,repo):
 #	link = 'https://api.github.com/repos/' + owner + '/' + repo + AUTH;
 #	print(link);
 	#reque = requests.get('https://api.github.com/repos/' + owner + '/' + repo + AUTH)
 	#reque = requests.get('https://api.github.com/repos/' + owner + '/' + repo + '/commits' + AUTH)
 	#reque = requests.get('https://api.github.com/repos/' + owner + '/' + repo + '/contributors' + AUTH)
-	dict_data = {}
+	repos = {}
+	contribuidores = []
+	reque = requests.get('https://api.github.com/repos/' + owner + '/' + repo + '/stats/contributors' + AUTH)
+	if reque.ok:
+		repoItem = json.loads(reque.text)
+		sum_weeks(repoItem)
+	return repoItem
 
-	while 1:
-		reque = requests.get('https://api.github.com/repos/' + owner + '/' + repo + '/stats/contributors' + AUTH)
-		if reque.ok:
-			repoItem = json.loads(reque.text);
-			import pdb;pdb.set_trace()
-			sum_weeks(repoItem);
-			dict_data[owner+'/'+repo] = getting_better_contributors(repoItem)
-			print(repoItem);
-			print(json.dumps(repoItem,indent=4,sort_keys=True));
-
-			#print(type(repoItem));
-			#print(repoItem);	
-		else:
-			print('fail');
-
-def getGraph(repo_list):
-	for repo in repo_list:
-		reque = requests.get('https://api.github.com/search/repositories?q='+repo)
+def return_repo(user):
+	reque = requests.get('https://api.github.com/users/'+user+'/repos' + AUTH)
+	return json.loads(reque.content)
 
 # getGraph(['tetris'])
-repository('nlohmann','json');
+# repository('nlohmann','json');
+# return_repo('nlohmann')
 # repository = raw_input('Digite o repositorio')
 # request_repository(repository)
