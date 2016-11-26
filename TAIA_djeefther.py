5a89ca5c6b80472122f2452a03bf9f01aaa5564a
Yago Zacarias Gomes Coutinho Ribeiro <yzgcr@ug4c22.windows.cin.ufpe.br>
2016-11-26 14:59:44 -0300

    criando as funçoes que retorna os repositorios que o usuario deu estrela

--- TAIA_djeefther.py
+++ TAIA_djeefther.py
@@ -39,13 +39,12 @@ def getting_better_contributors(repoItem):
 	return list_sorted[:2]
 
 def return_contri(owner,repo):
+	""" recebe um usuario e um repositorio e retorna os contribuidores desse repositorio """
 #	link = 'https://api.github.com/repos/' + owner + '/' + repo + AUTH;
 #	print(link);
 	#reque = requests.get('https://api.github.com/repos/' + owner + '/' + repo + AUTH)
 	#reque = requests.get('https://api.github.com/repos/' + owner + '/' + repo + '/commits' + AUTH)
 	#reque = requests.get('https://api.github.com/repos/' + owner + '/' + repo + '/contributors' + AUTH)
-	repos = {}
-	contribuidores = []
 	reque = requests.get('https://api.github.com/repos/' + owner + '/' + repo + '/stats/contributors' + AUTH)
 	if reque.ok:
 		repoItem = json.loads(reque.text)
@@ -53,8 +52,21 @@ def return_contri(owner,repo):
 	return repoItem
 
 def return_repo(user):
+	""" retorna uma lista de dicionarios dos repositorios do usuario """
 	reque = requests.get('https://api.github.com/users/'+user+'/repos' + AUTH)
 	return json.loads(reque.content)
+
+def return_starred(user):
+	""" retorna uma lista de dicionarios dos repositorios que o usuario deu estrela """
+	reque = requests.get('https://api.github.com/users/'+user+'/starred' + AUTH)
+	https://github.com/nlohmann?tab=stars
+	return json.loads(reque.content)
+
+def return_repo_starred(user):
+	""" retorna duas listas de dicionarios. A primeira sao os repositorios do usuario e a segunda a que ele deu estrela """
+	repo = requests.get('https://api.github.com/users/'+user+'/repos' + AUTH)
+	starred = requests.get('https://api.github.com/users/'+user+'/starred' + AUTH)
+	return json.loads(repo.content), json.loads(starred.content)
 
 # getGraph(['tetris'])
 # repository('nlohmann','json');
