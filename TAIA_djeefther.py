2f349a5732c63b838d80d7a1adff46c3aa20046b
Yago Zacarias Gomes Coutinho Ribeiro <yzgcr@ug4c22.windows.cin.ufpe.br>
2016-11-26 13:56:49 -0300

    pegando os melhores contribuidores

--- TAIA_djeefther.py
+++ TAIA_djeefther.py
@@ -12,13 +12,14 @@ def request_repository(repository):
 			constributors_get = requests.get(project['contributors_url']+AUTH)
 			if constributors_get.text:
 				constributors = json.loads(constributors_get.text or constributors_get.content)
-				for contributor in constributors:
-					print contributor['login'],contributor['contributions']
-				print '\n'
+				import pdb;pdb.set_trace()
+				# for contributor in constributors:
+				# 	print contributor['login'],contributor['contributions']
+				# print '\n'
 	else:
 		print 'fuu'
 		# print "Django repository created: " + repoItem['created_at']
-		
+	
 
 def sum_weeks(repoItem_list_contr):
 	for contri in repoItem_list_contr:
@@ -31,6 +32,12 @@ def sum_weeks(repoItem_list_contr):
 		contri["weeks"] = acc_weeks;
 	return 	repoItem_list_contr;
 
+def getting_better_contributors(repoItem):
+	list_contri = [] 
+	for contri in repoItem:
+		list_contri.append((contri['author']['login'],contri['author']['url'],contri['weeks']['a'] + contri['weeks']['d']))
+	list_sorted = sorted(list_contri, key=lambda tup: tup[2], reverse=True)
+	return list_sorted[:2]
 
 def repository(owner,repo):
 #	link = 'https://api.github.com/repos/' + owner + '/' + repo + AUTH;
@@ -38,20 +45,28 @@ def repository(owner,repo):
 	#reque = requests.get('https://api.github.com/repos/' + owner + '/' + repo + AUTH)
 	#reque = requests.get('https://api.github.com/repos/' + owner + '/' + repo + '/commits' + AUTH)
 	#reque = requests.get('https://api.github.com/repos/' + owner + '/' + repo + '/contributors' + AUTH)
-	reque = requests.get('https://api.github.com/repos/' + owner + '/' + repo + '/stats/contributors' + AUTH)
+	dict_data = {}
 
+	while 1:
+		reque = requests.get('https://api.github.com/repos/' + owner + '/' + repo + '/stats/contributors' + AUTH)
+		if reque.ok:
+			repoItem = json.loads(reque.text);
+			import pdb;pdb.set_trace()
+			sum_weeks(repoItem);
+			dict_data[owner+'/'+repo] = getting_better_contributors(repoItem)
+			print(repoItem);
+			print(json.dumps(repoItem,indent=4,sort_keys=True));
 
-	if reque.ok:
-		repoItem = json.loads(reque.text);
-		sum_weeks(repoItem);
-		print(repoItem);
-		print(json.dumps(repoItem,indent=4,sort_keys=True));
+			#print(type(repoItem));
+			#print(repoItem);	
+		else:
+			print('fail');
 
-		#print(type(repoItem));
-		#print(repoItem);	
-	else:
-		print('fail');
+def getGraph(repo_list):
+	for repo in repo_list:
+		reque = requests.get('https://api.github.com/search/repositories?q='+repo)
 
+# getGraph(['tetris'])
 repository('nlohmann','json');
-#repository = raw_input('Digite o repositorio')
-#request_repository(repository)
+# repository = raw_input('Digite o repositorio')
+# request_repository(repository)
