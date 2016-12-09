15e96a5da70da0f8ed0b3778e5d427c540286edb
YagoGomes <yzgcr@cin.ufpe.br>
2016-12-09 19:32:29 -0300

    link prediction

--- graph.py
+++ graph.py
@@ -53,7 +53,7 @@ def create_format_graph(index):
 					contributors_json = open(index[contributors_url],'r')
 				except KeyError:
 					# import pdb;pdb.set_trace()
-					print contributors_url
+					# print contributors_url
 					error +=1
 					continue
 				projects[repo['url']] = []
@@ -61,10 +61,35 @@ def create_format_graph(index):
 				contributors = json.load(contributors_json)
 				for contributor in contributors:
 					projects[repo['url']].append((contributor['login'],contributor['contributions']))
-	print error
+	# print error
 
 	return projects
 
+
+
+def link_prediction(graph_dict,graph,repo):
+	users = []
+	for tupla in graph_dict[repo]:
+		users.append(tupla[0])
+
+	repos = []
+	for key in graph_dict:
+		if key != repo:
+			repos.append(key)
+
+	maybe_contri = []
+	contri_list = []
+	for repo_p in repos:
+		for user in users:
+			for contri,cont in graph_dict[repo_p]:
+				if contri == user:
+					user_stats = max(graph_dict[repo_p],key=operator.itemgetter(1))
+					if user_stats[0] not in maybe_contri and user_stats[0] != user:
+						maybe_contri.append(user_stats[0])
+						contri_list.append(user_stats[1])
+
+
+	import pdb;pdb.set_trace()
 
 directory  = 'cache';
 
@@ -83,9 +108,11 @@ for key,lista in graph_dict.iteritems():
 		graph_list.append((key,contributor,contributions))
 
 contributor_by_contributions, contributor_by_edge = most_important_contributors(graph_list)
-
+print "Contribuidor por contribuicao: " + contributor_by_contributions
+print "Contribuiu para mais projetos: " + contributor_by_edge
 B = nx.Graph()
 B.add_weighted_edges_from(graph_list)
+link_prediction(graph_dict,graph_list,'https://api.github.com/repos/nlohmann/json')
 X, Y = bipartite.sets(B)
 pos = dict()
 pos.update( (n, (1, i)) for i, n in enumerate(X) ) # put nodes from X at x=1
